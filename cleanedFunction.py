397c24f92903898a079a2e9eff1fe42df690bff0
d.riccio9 <d.riccio9@studenti.unisa.it>
2021-12-01 16:08:07 +0100

    aggiornata funzione cleaned function

--- cleanedFunction.py
+++ cleanedFunction.py
@@ -1,22 +1,14 @@
 import re
+import html
 
 x = 'RT @fausername1: @fakeusername2 just bought 30.6 BTC from https://fakeesceafafa.com &amp; I am hating the crash\n Hope it recovers so... '
-#rimozione ...
-x = re.sub("\\.\\.\\.", '', x)
-print(x)
-#parseHTML chars
 
-# import html
-import html
-x = html.unescape(x)
-print(x)
+def cleaned(text):
+    text = re.sub(
+        "(#\S+)|(RT @\w+:)|(@\S+)|([+-]?[0-9]+)|([-+]?[0-9]*.[0-9]+)|(http\S+)|(\.\.\.)",
+        '',
+        html.unescape(text)
+    )
+    return text.lower()
 
-#remove hyperlinks
-x = re.sub("http\S+", '', x)
-print(x)
-
-#remove numbers
-x = re.sub("([+-]?[0-9]+)|([-+]?[0-9]*.[0-9]+)", '', x)
-print(x)
-
-#remove mentions, hashtags and retweet tags
+print(cleaned(x))
