2f6785c1de719f688ce7b604120f6cbfb54f290a
d.riccio9 <d.riccio9@studenti.unisa.it>
2021-12-17 09:08:26 +0100

    modificato piechart.py e text2emotionSentiment.py

--- text2emotionSentiment.py
+++ text2emotionSentiment.py
@@ -1,27 +1,18 @@
 import text2emotion as te
 import pandas as pd
-import matplotlib.pyplot as plt
 
-df = pd.read_json(r'sample.json')
-df['emotions'] = df.cleanedEnglishText.map(te.get_emotion)
 
-meanEmotions = {
-    'Happy': .0,
-    'Angry': .0,
-    'Surprise': .0,
-    'Sad': .0,
-    'Fear': .0
-}
+def text2emotion(dataFrame:pd.DataFrame, outPathFile:str):
+    emotions = dataFrame['text'].map(te.get_emotion).tolist()
+    dataFrame = pd.concat([dataFrame, pd.DataFrame(emotions)], axis=1)
+    dataFrame.to_json(outPathFile, orient='records')
 
-for row in df['emotions']:
-    for k, v in row.items():
-        meanEmotions[k] += (v / df.shape[0]) * 100
 
-#pie cahart
-labels = ['Happy', 'Angry', 'Surprise', 'Sad', 'Fear']
-values = [meanEmotions['Happy'], meanEmotions['Angry'], meanEmotions['Surprise'], meanEmotions['Sad'], meanEmotions['Fear']]
-patches, texts, junk = plt.pie(values, labels=labels,  startangle=90, autopct='%.2f')
-plt.style.use('default')
-plt.title("Text to Emotion Result")
-plt.axis('equal')
-plt.show()
+#Prova#########
+df = pd.read_json(r'alltweet.json', date_unit='s')
+text2emotion(df, r'sample/text2emotionAllTweet.json')
+################
+
+inputsFiles = [
+    ''
+]
