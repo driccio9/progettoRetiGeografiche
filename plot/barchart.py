badaedb52dcfb608986bc3886cdbd62cd53912f8
d.riccio9 <d.riccio9@studenti.unisa.it>
2021-12-21 13:35:29 +0100

    aggiornato barchart.py con barchartVader()

--- plot/barchart.py
+++ plot/barchart.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 def barchartText2Emotion():
     prefix = r"..\sample\T2E_"
     postfix = ".json"
@@ -61,4 +62,69 @@ def barchartText2Emotion():
     plt.legend((p[0][0], p[1][0], p[2][0], p[3][0], p[4][0]), ('happy', 'angry', 'surprise', 'sad', 'fear'))
     plt.show()
 
-barchartText2Emotion()
+
+def barchartVader():
+    prefix = r"..\sample\VADER_"
+    postfix = ".json"
+    words = [
+        "(#supergreenpass OR #greenpassrafforzato OR #obbligovaccinale OR #vaccinoobbligatorio) (salvini OR lega)",
+        "(#supergreenpass OR #greenpassrafforzato OR #obbligovaccinale OR #vaccinoobbligatorio) (meloni OR fdi OR fratellidiitalia)",
+        "(#supergreenpass OR #greenpassrafforzato OR #obbligovaccinale OR #vaccinoobbligatorio) (m5s OR dimaio OR conte OR maio)",
+        "(#supergreenpass OR #greenpassrafforzato OR #obbligovaccinale OR #vaccinoobbligatorio) (renzi OR italiaviva)",
+        "(#supergreenpass OR #greenpassrafforzato OR #obbligovaccinale OR #vaccinoobbligatorio) (berlusconi OR forzaitalia)",
+        "(#supergreenpass OR #greenpassrafforzato OR #obbligovaccinale OR #vaccinoobbligatorio) (bersani OR articolouno)",
+        "(#supergreenpass OR #greenpassrafforzato OR #obbligovaccinale OR #vaccinoobbligatorio) (letta OR pd)"
+    ]
+
+    labels = (
+        "Lega",
+        "FDI",
+        "M5S",
+        "IV",
+        "FI",
+        "A1",
+        "PD"
+    )
+
+    sentimentMeanList = []
+
+    for i in range(0, len(words)):
+        words[i] = prefix + words[i] + postfix
+
+        #recupero per ogni partito politico il vettore della media dei sentimenti
+        sentimentMean = pd.read_json(words[i],  convert_dates=False)[['negative', 'positive', 'neutral']].mean(axis=0)
+
+        #sentimentMean = sentimentMean / np.linalg.norm(sentimentMean, ord=1)
+
+        sentimentMeanList.append(sentimentMean*100)
+
+
+    sentimentMeanMatrix = np.array(sentimentMeanList)
+
+    fig = plt.subplots(figsize=(10, 7))
+    plt.grid(color='green', linestyle='--', linewidth=0.5, axis='y')
+
+    ind = np.arange(7)
+    width = 0.35
+
+    #list of bar
+    p = []
+
+    cumSum = sentimentMeanMatrix.cumsum(axis=1)
+    p.append(plt.bar(ind, sentimentMeanMatrix[:, 0], width))
+
+    for i in range(1, sentimentMeanMatrix.shape[1]):
+        p.append(plt.bar(ind, sentimentMeanMatrix[:, i], width, bottom=cumSum[:, i-1]))
+
+    plt.ylabel('sentiment %')
+    plt.title('sentiment by political party (VADER)')
+    plt.xticks(ind, labels)
+    plt.yticks(np.arange(0, 105, 5))
+
+    plt.legend((p[0][0], p[1][0], p[2][0]), ('negative', 'positive', 'neutral'))
+    plt.show()
+
+
+
+#barchartText2Emotion()
+barchartVader()
--- prove/proveDavide.py
+++ prove/proveDavide.py
@@ -3,7 +3,7 @@ from googletrans import Translator
 
 #x = 'RT @fausername1: @fakeusername2 just bought 30.6 BTC from https://fakeesceafafa.com &amp; I am hating the crash\n Hope it recovers so... '
 
-
+"""
 x = "@matteosalvinimi @mariogiordano5 Cosa hai fatto per evitargli la Censura? Nulla come per evitare a noi il #supergreenpass. Scompari #Lega"
 x = cleaned(x)
 
@@ -11,5 +11,7 @@ translator = Translator()
 x = translator.translate(x, dest='en', src='it').text
 print(x)
 print(cleaned(x))
+"""
+
 
 
