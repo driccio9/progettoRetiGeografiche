1c01bb962473c72a76e5fbe310127c5f72315288
d.riccio9 <d.riccio9@studenti.unisa.it>
2021-12-22 11:54:39 +0100

    push

--- text2emotionSentiment.py
+++ text2emotionSentiment.py
@@ -1,11 +1,54 @@
 import text2emotion as te
 import pandas as pd
+import json
+from cleanedFunction import *
+import numpy as np
+
 
 #il testo deve essere tradotto in linugua inglese
 def text2emotion(dataFrame:pd.DataFrame, outPathFile):
     emotions = dataFrame['text'].map(te.get_emotion).tolist()
     dataFrame = pd.concat([dataFrame, pd.DataFrame(emotions)], axis=1)
     dataFrame.to_json(outPathFile, orient='records')
+
+
+def text2emotionSentence(inPathFile, outPathFile, mode='nltk'):
+    valid = {'nltk', 'regex'}
+    if mode not in valid:
+        raise ValueError("mode must be one of %r." % valid)
+
+    f = open(inPathFile, "r")
+    inTweetList = json.loads(f.read())
+    f.close()
+
+    outTweetList = []
+    for tweet in inTweetList:
+
+        if mode == 'nltk':
+            sentenceList = nltkSentenceSplit(tweet['text'])
+
+        emotion = np.zeros(5)
+        for sentence in sentenceList:
+            emotion += np.array(list(te.get_emotion(remove_stopwords(sentence)).values())) / len(sentenceList)
+
+        emotion /= np.linalg.norm(emotion, ord=1)
+
+        outTweetList.append(
+            {
+                'text': tweet['text'],
+                'date': tweet['date'],
+                'Happy': emotion[0],
+                'Angry': emotion[1],
+                'Surprise': emotion[2],
+                'Sad': emotion[3],
+                'Fear': emotion[4]
+            }
+        )
+
+
+    f = open(outPathFile, "w")
+    f.write(json.dumps(outTweetList))
+    f.close()
 
 """
 #Prova#########
@@ -14,6 +57,7 @@ text2emotion(df, r'sample/text2emotionAllTweet.json')
 ################
 """
 
+"""
 prefix = r'sample\TRANSLATED_'
 postfix = '.json'
 outPrefix = r'sample\T2E_'
@@ -31,5 +75,6 @@ words = [
 for i in range(0,len(words)):
     df = pd.read_json(prefix+words[i]+postfix, convert_dates=False)
     text2emotion(df, outPrefix+words[i]+postfix)
+"""
 
 
