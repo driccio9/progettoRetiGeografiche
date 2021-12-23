f59e436f622deb529b0887566c5940cc6b9290be
d.riccio9 <d.riccio9@studenti.unisa.it>
2021-12-23 11:17:16 +0100

    Aggiornato sentimentMain.py, funziona ma è configurato sui file di sample.

--- SentimentAnalysisTools/sentimentMain.py
+++ SentimentAnalysisTools/sentimentMain.py
@@ -1,4 +1,44 @@
-dirInput = "../Tweet"
-dirOutput = "../SentimentAnalysisToolsOutput"
+from SentimentAnalysisTools.text2emotionSentiment import *
+from SentimentAnalysisTools.textBlobSentiment import *
+from SentimentAnalysisTools.vaderSentiment import *
 
+dirInput ="sample/" #"../Tweet/"
+dirOutput = "SentimentAnalysisToolsOutput/"
+
+prefixInput = "TRANSLATED_"
+
+prefixOutput = ["T2E_", "TB_", "VADER_"]
+mode = ["none", "nltk", "regex"]
+analysisTools = [text2emotionSentiment, textBlobSentiment, vaderSentiment]
+
+postfix = ".json"
 #WIP...
+
+#per questioni di prove manca l'hashtag #greenpass. Attenzione!
+words = [
+    "(#supergreenpass OR #greenpassrafforzato OR #obbligovaccinale OR #vaccinoobbligatorio) (salvini OR lega)",
+    "(#supergreenpass OR #greenpassrafforzato OR #obbligovaccinale OR #vaccinoobbligatorio) (meloni OR fdi OR fratellidiitalia)",
+    "(#supergreenpass OR #greenpassrafforzato OR #obbligovaccinale OR #vaccinoobbligatorio) (m5s OR dimaio OR conte OR maio)",
+    "(#supergreenpass OR #greenpassrafforzato OR #obbligovaccinale OR #vaccinoobbligatorio) (renzi OR italiaviva)",
+    "(#supergreenpass OR #greenpassrafforzato OR #obbligovaccinale OR #vaccinoobbligatorio) (berlusconi OR forzaitalia)",
+    "(#supergreenpass OR #greenpassrafforzato OR #obbligovaccinale OR #vaccinoobbligatorio) (bersani OR articolouno)",
+    "(#supergreenpass OR #greenpassrafforzato OR #obbligovaccinale OR #vaccinoobbligatorio) (letta OR pd)"
+]
+
+for analysisToolsIndex in range(0, len(analysisTools)):
+    for modeIndex in range(0, len(mode)):
+        for word in words:
+            inputJson = dirInput + prefixInput + word + postfix
+            outputJson = dirOutput + prefixOutput[analysisToolsIndex] + mode[modeIndex] + "_" + word + postfix
+            print(inputJson)
+            print(outputJson)
+            analysisTools[analysisToolsIndex](inputJson, outputJson, mode[modeIndex])
+
+
+
+
+
+
+
+
+
